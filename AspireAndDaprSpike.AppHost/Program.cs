e28f5e75bbde46b95ea05aaaf044ca55cf481f0d
Joe Whiteaker <47674962+josephaw1022@users.noreply.github.com>
2024-03-17 00:51:41 -0400

    finished microservice-2

--- AspireAndDaprSpike.AppHost/Program.cs
+++ AspireAndDaprSpike.AppHost/Program.cs
@@ -2,29 +2,40 @@ using Aspire.Hosting.Dapr;
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var postgresDatabaseServer = builder.AddPostgres("PostgresDB", 5432, "DaprPostgresPassword")
-        .WithPgAdmin()
-        .WithBindMount("./postgres-config/config-store", "/docker-entrypoint-initdb.d");
-
-
-var configurationDatabase = postgresDatabaseServer.AddDatabase("ConfigurationDB");
-
-
-var configurationStore = builder.AddDaprComponent("config-store-1", "configuration", new()
+// Dapr components
+var configurationStore1 = builder.AddDaprComponent("config-store-1", "configuration", new()
 {
-    LocalPath = "./dapr/ConfigStore.yaml",
+    LocalPath = "./dapr/ConfigStore1.yaml",
 });
 
+var configurationStore2 = builder.AddDaprComponent("config-store-2", "configuration", new()
+{
+    LocalPath = "./dapr/ConfigStore2.yaml",
+});
 
-var microservice1 = builder.AddProject<Projects.microservice_01>("microservice01")
-    .WithDaprSidecar(new DaprSidecarOptions
+var dashboard = builder.AddExecutable("dapr-dashboard", "dapr", ".", "dashboard" )
+    .WithHttpEndpoint(containerPort: 8080, hostPort: 8080, name:"dashboard-http", isProxied: false);
+
+
+// Dapr sidecar options
+static DaprSidecarOptions CreateDaprSidecarOptions(string appId)
+{
+    return new()
     {
-        AppHealthThreshold = 15,
-    })
-    .WithReference(configurationStore);
+        AppHealthThreshold = 20,
+        AppId = appId,
+    };
+}
 
+// Microservices
 
+builder.AddProject<Projects.microservice_01>("microservice01")
+    .WithDaprSidecar(CreateDaprSidecarOptions("microservice01-dapr"))
+    .WithReference(configurationStore1);
 
+builder.AddProject<Projects.microservice_02>("microservice02")
+    .WithDaprSidecar(CreateDaprSidecarOptions("microservice02-dapr"))
+    .WithReference(configurationStore2);
 
-
-builder.Build().Run();
+// Start the applications
+await builder.Build().RunAsync();
---
+++ microservice-02/Program.cs
@@ -0,0 +1,37 @@
+using Dapr.Client;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.AddServiceDefaults();
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddDaprClient();
+
+
+var app = builder.Build();
+
+app.MapDefaultEndpoints();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.MapGet("/", async (DaprClient daprClient) =>
+{
+    var configValues = await daprClient.GetConfiguration("config-store-2", new List<string>() {});
+
+    return Results.Ok(configValues);
+})
+.WithName("Configure Store 2 Items")
+.WithOpenApi();
+
+
+app.Run();
